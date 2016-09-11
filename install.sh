68a2315e814501866303bb56ae429175bcf7cedb
Zeno Zeng <zenoofzeng@gmail.com>
2016-09-12 01:00:41 +0800

    -b v1.0.0

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /tmp
-git clone -b $(curl -L http://grpc.io/release) https://github.com/grpc/grpc
+git clone -b v1.0.0 https://github.com/grpc/grpc
 cd /tmp/grpc
 git submodule update --init
 make
